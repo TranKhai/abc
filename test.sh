a5a3cd1b9ab0b95d3187266307c620d1cc412f57
Khai Tran <hongngoctqk9@gmail.com>
2022-04-05 15:01:12 +0700

    add shell 1

--- test.sh
+++ test.sh
@@ -2,11 +2,10 @@
 
 echo 'Star build'
 
-ssh -i /var/jenkins_home/.ssh/id_rsa root@172.17.0.3
 apt update
 apt install -y npm
 git clone https://github.com/TranKhai/abc.git
-cd abc
+cd abc/
 npm install
 npm run build
 npm start
